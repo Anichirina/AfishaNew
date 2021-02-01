b1f891ca4527a5d3af17a62d89590768bf2d1d4c
Irina Anichkova <anichkova2016@yandex.ru>
2021-02-01 22:34:38 +0300

    Change MovieManadger, MovieManagerTest

--- src/test/java/ru/netology/manadger/MovieManagerTest.java
+++ src/test/java/ru/netology/manadger/MovieManagerTest.java
@@ -1,11 +1,13 @@
 package ru.netology.manadger;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.MovieItem;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class MovieManagerTest {
+    MovieManager manager = new MovieManager();
     private final MovieItem film1 = new MovieItem(1, 1, "Happy", "comedy", 2000);
     private final MovieItem film2 = new MovieItem(2, 2, "Love", "melodrama", 2005);
     private final MovieItem film3 = new MovieItem(3, 3, "Boxer", "triller", 1990);
@@ -16,12 +18,11 @@ class MovieManagerTest {
     private final MovieItem film8 = new MovieItem(8, 8, "Ono", "horror", 2020);
     private final MovieItem film9 = new MovieItem(9, 9, "Road", "western", 2010);
     private final MovieItem film10 = new MovieItem(10, 10, "Smile", "comedy", 2019);
-    private final MovieItem film11 = new MovieItem(11, 11, "Kiss", "melodrama", 2010);
+    private final MovieItem film11 = new MovieItem(33, 11, "Kiss", "melodrama", 2010);
 
-
-    @Test
-    public void shouldGetFastFilm() {
-        MovieManager manager = new MovieManager();
+    @BeforeEach
+    public void setup() {
+        manager.add(film1);
         manager.add(film11);
         manager.add(film2);
         manager.add(film3);
@@ -32,54 +33,35 @@ class MovieManagerTest {
         manager.add(film8);
         manager.add(film9);
         manager.add(film10);
+    }
 
-        MovieItem[] actual = manager.getFast();
-        MovieItem[] expected = new MovieItem[]{film11, film2, film3, film4, film5, film6, film7, film8, film9, film10};
-
-
+    @Test
+    public void shouldGetFastFilm() {
+        MovieItem[] actual = manager.getFast(10);
+        MovieItem[] expected = new MovieItem[]{film10, film9, film8, film7, film6, film5, film4, film3, film2, film11};
         assertArrayEquals(expected, actual);
     }
     @Test
     public void ReturnLessThan10() {
-        MovieManager manager = new MovieManager();
-        manager.add(film1);
-        manager.add(film2);
-        manager.add(film3);
-        manager.add(film4);
-        manager.add(film5);
-        manager.add(film6);
-        manager.add(film7);
-        manager.add(film8);
 
-        MovieItem[] actual = manager.getFast();
-        MovieItem[] expected = new MovieItem[]{film8, film7, film6, film5, film4, film3, film2, film1};
+        MovieItem[] actual = manager.getFast(8);
+        MovieItem[] expected = new MovieItem[]{film10, film9, film8, film7, film6, film5, film4, film3};
 
         assertArrayEquals(expected, actual);
     }
     @Test
     public void Return4() {
-        MovieManager manager = new MovieManager(4);
-        manager.add(film1);
-        manager.add(film2);
-        manager.add(film3);
-        manager.add(film4);
-        manager.add(film5);
 
-        MovieItem[] actual = manager.getFast();
-        MovieItem[] expected = new MovieItem[]{film5, film4, film3, film2};
+        MovieItem[] actual = manager.getFast(4);
+        MovieItem[] expected = new MovieItem[]{film10, film9, film8, film7};
 
         assertArrayEquals(expected, actual);
     }
     @Test
     public void ReturnLessThan5() {
-        MovieManager manager = new MovieManager(4);
-        manager.add(film1);
-        manager.add(film2);
-        manager.add(film3);
-        manager.add(film4);
 
-        MovieItem[] actual = manager.getFast();
-        MovieItem[] expected = new MovieItem[]{film4, film3, film2, film1};
+        MovieItem[] actual = manager.getFast(4);
+        MovieItem[] expected = new MovieItem[]{film10, film9, film8, film7};
 
         assertArrayEquals(expected, actual);
     }
